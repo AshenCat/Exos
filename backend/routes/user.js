30ac9885cb989713f1b7d927261075c001e217bc
Klifford Agujar <forddagujar95@gmail.com>
2020-06-28 22:15:37 -0400

    added working user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -8,14 +8,28 @@ const model = require('../models/user');
 server.route('/manage')
     // return
     .get((req,res,next)=>{
-
+        model.find({}, (err, data) => {
+            if(err) {
+                res.status(500);
+                next(err);
+            } else if (data === null) {
+                res.json({msg:"not found", payload: null})
+            } else{
+                res.json({
+                    message: "",
+                    payload: data
+                })
+            }
+        })
     })
     .put((req,res,next) =>{
-
+        res.send('put')
     })
     .patch((req,res,next)=>{
-
+        res.send('patch')
     })
     .delete((req,res,next)=>{
-
+        res.send('delete')
     })
+
+module.exports = server;
