de680c522b4b4618dc32504d6f5baa77eb75712d
Klifford Agujar <forddagujar95@gmail.com>
2020-06-29 21:53:40 -0400

    added unique and required constraint

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,9 +3,22 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let userSchema = new Schema({
-    username: String,
-    password: String,
-    email: String,
-})
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+}, 
+{timestamps: true}
+)
 
 module.exports = mongoose.model('Users', userSchema);
