264a5cf11ac1d5a1d7871c3d4e685411998ddc30
Klifford Agujar <forddagujar95@gmail.com>
2020-07-01 00:29:43 -0400

    modified data format

--- src/components/Content.js
+++ src/components/Content.js
@@ -18,11 +18,11 @@ class Content extends React.Component {
             race: "Dwarf",
             position: "Defense",
             type: "Physical",
-            description: "The first of the Five Greatest Generals of Saint West. While Garff was born a commoner, he was always interested in war and dreamed of becoming a general. To realize his dream, Garff studied strategies and writings of great commanders.\u00A0 \u00A0When he was old enough, Garff joined the army. He earned respect through self-management and Outstanding abilities, despite his humble background. Valarr, who had taken an interest in Garff, recommended him for the knighthood.\u00A0 \u00A0Though Garff became a knight at a relatively late age, he was able to rise to the rank of general in a short time after defeating Valarr in a jousting competition and gaining the favor of the King. After becoming a general, Garff made a name for himself from his contributions in the war against Bronn and became the First of the Five Greatest Generals.\u00A0 \u00A0However, the King was killed in a revolt led by Carrie and the capital fell into the rebels. Now Garff is avoiding Valarr's pursuit and gathering forces in preparation for Rachel's return.",
+            description: "The first of the Five Greatest Generals of Saint West. While Garff was born a commoner, he was always interested in war and dreamed of becoming a general. To realize his dream, Garff studied strategies and writings of great commanders.\n\nWhen he was old enough, Garff joined the army. He earned respect through self-management and Outstanding abilities, despite his humble background. Valarr, who had taken an interest in Garff, recommended him for the knighthood.\n \nThough Garff became a knight at a relatively late age, he was able to rise to the rank of general in a short time after defeating Valarr in a jousting competition and gaining the favor of the King. After becoming a general, Garff made a name for himself from his contributions in the war against Bronn and became the First of the Five Greatest Generals.\n \nHowever, the King was killed in a revolt led by Carrie and the capital fell into the rebels. Now Garff is avoiding Valarr's pursuit and gathering forces in preparation for Rachel's return.",
             skills: {
-                passive: { name: "Protect 3", description: "Grants Share Health to ally with the lowest maximum Health and heals their Health by 30% of the caster's current Health at the beginning of the round\u00A0Gain Mana(1 mana) if Health of the target is less than 20%\u00A0Gain(1 mana) if Health of target is more than 70%\u00A0\u00A0[Breath of Mana]\u00A0[Over Time Effect] Front row allies permanently gain 1 mana\u00A0\u00A0[Command]\u00A0Grants allies with lower maximum Health than self with the Comman mark and increases Defense/Health by 20%"},
-                active1: { name: "Eagle Blow", cost: 2, description: "[Single] Deals 375% damage to 1 enemy"},
-                active2: { name: "General's Call", cost: 5, description: "[Back Row First] Deals 420% damage to all back row enemies"}
+                passive: { name: "Protect 3", description: ["Grants Share Health to ally with the lowest maximum Health and heals their Health by 30% of the caster's current Health at the beginning of the round\nGain Mana(1 mana) if Health of the target is less than 20%\nGain(1 mana) if Health of target is more than 70%", "[Breath of Mana]\n[Over Time Effect] Front row allies permanently gain 1 mana", "[Command]\nGrants allies with lower maximum Health than self with the Comman mark and increases Defense/Health by 20%"]},
+                active1: { name: "Eagle Blow", cost: 2, description: ["[Single] Deals 375% damage to 1 enemy"]},
+                active2: { name: "General's Call", cost: 5, description: ["[Back Row First] Deals 420% damage to all back row enemies"]}
             },
             maxStats: {
                 level: 100,
@@ -40,7 +40,8 @@ class Content extends React.Component {
                 criticalDamage: 150,
                 blockDefenceRate: 50,
                 luck: 0
-            }
+            },
+            trivias: []
         },
         {
             _id: 2,
@@ -56,7 +57,7 @@ class Content extends React.Component {
             type: "Physical",
             description: "King's Guard of Saint West",
             skills: {
-                passive: { name: "Protect 3", description: "Grants Share Health to ally with the lowest maximum Health and heals their Health by 30% of the caster's current Health at the beginning of the round\u00A0Gain Mana(1 mana) if Health of the target is less than 20%\u00A0Gain(1 mana) if Health of target is more than 70%\u00A0\u00A0[Breath of Mana]\u00A0[Over Time Effect] Front row allies permanently gain 1 mana\u00A0\u00A0[Command]\u00A0Grants allies with lower maximum Health than self with the Comman mark and increases Defense/Health by 20%"},
+                passive: { name: "Protect 3", description: "Grants Share Health to ally with the lowest maximum Health and heals their Health by 30% of the caster's current Health at the beginning of the round\nGain Mana(1 mana) if Health of the target is less than 20%\nGain(1 mana) if Health of target is more than 70%\n\n[Breath of Mana]\n[Over Time Effect] Front row allies permanently gain 1 mana\n\n[Command]\nGrants allies with lower maximum Health than self with the Comman mark and increases Defense/Health by 20%"},
                 active1: { name: "Eagle Blow", cost: 2, description: "[Single] Deals 375% damage to 1 enemy"},
                 active2: { name: "General's Call", cost: 5, description: "[Back Row First] Deals 420% damage to all back row enemies"}
             },
@@ -92,7 +93,7 @@ class Content extends React.Component {
             type: "Magical",
             description: "King's Guard of Green Land",
             skills: {
-                passive: { name: "Protect 3", description: "Grants Share Health to ally with the lowest maximum Health and heals their Health by 30% of the caster's current Health at the beginning of the round\u00A0Gain Mana(1 mana) if Health of the target is less than 20%\u00A0Gain(1 mana) if Health of target is more than 70%\u00A0\u00A0[Breath of Mana]\u00A0[Over Time Effect] Front row allies permanently gain 1 mana\u00A0\u00A0[Command]\u00A0Grants allies with lower maximum Health than self with the Comman mark and increases Defense/Health by 20%"},
+                passive: { name: "Protect 3", description: "Grants Share Health to ally with the lowest maximum Health and heals their Health by 30% of the caster's current Health at the beginning of the round\nGain Mana(1 mana) if Health of the target is less than 20%\nGain(1 mana) if Health of target is more than 70%\n\n[Breath of Mana]\n[Over Time Effect] Front row allies permanently gain 1 mana\n\n[Command]\nGrants allies with lower maximum Health than self with the Comman mark and increases Defense/Health by 20%"},
                 active1: { name: "Eagle Blow", cost: 2, description: "[Single] Deals 375% damage to 1 enemy"},
                 active2: { name: "General's Call", cost: 5, description: "[Back Row First] Deals 420% damage to all back row enemies"}
             },
