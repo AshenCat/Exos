fd6541a63c3d240f7114ae74407705cfe2cb3b71
Klifford Agujar <forddagujar95@gmail.com>
2020-07-01 00:30:09 -0400

    removed spinner dependency

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Navbar, Nav, NavDropdown, Form, InputGroup, Col, Button, Spinner } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Form, InputGroup, Col, Button } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 const Header = () => {
