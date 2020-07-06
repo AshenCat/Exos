d978824836f1d156f4617339bf4ef5730a701661
Klifford Agujar <forddagujar95@gmail.com>
2020-07-05 23:19:43 -0400

    addded button, stats and other info

--- src/components/pages/ViewCharacter.js
+++ src/components/pages/ViewCharacter.js
@@ -18,6 +18,9 @@ class ViewCharacter extends Component {
         const character = this.props.character;
         return(
             <Container className="mt-4">
+              <Row className="flex-row-reverse">
+                <div></div><Button className="mr-4 mb-3" size="lg" variant="outline-secondary">Back</Button>
+              </Row>
               <Row>
                 <Col sm={3} style={{}}>
                   <h2 className="text-center">{character.name}</h2>
@@ -27,7 +30,9 @@ class ViewCharacter extends Component {
                   </div>
                 </Col>
                 <Col>
-                <Accordion defaultActiveKey="0">
+                
+                <Row>
+                  <Accordion defaultActiveKey="0" style={{"width": "100%"}}>
                   <Card>
                     <Card.Header>
                       <Accordion.Toggle as={Button} variant="link" eventKey="0">
@@ -72,11 +77,11 @@ class ViewCharacter extends Component {
                   <Card>
                     <Card.Header>
                       <Accordion.Toggle as={Button} variant="link" eventKey="2">
-                        Bio
+                        Other info
                       </Accordion.Toggle>
                     </Card.Header>
                     <Accordion.Collapse eventKey="2">
-                      <Card.Body>
+                      <Card.Body className="text-center">
                         {/*
                         name: "Garff",
                         role: "Paladin",
@@ -90,66 +95,99 @@ class ViewCharacter extends Component {
                         type: "Physical",
                         */}
                         <Row>
-                          <Col>
-                            <Card className="m-3">
-                              <Card.Header className="text-center">
-                                Role
-                              </Card.Header>
-                              <Card.Body className="text-center">
-                                {character.role}
-                              </Card.Body>
-                            </Card>
-                          </Col>
-                          <Col>
-                            <Card className="m-3">
-                              <Card.Header className="text-center">
-                                Sex
-                              </Card.Header>
-                              <Card.Body className="text-center">
-                                {character.sex}
-                              </Card.Body>
-                            </Card>
-                          </Col>
+                          <Col md={6}>Role: &nbsp;&nbsp;&nbsp;&nbsp;{character.role}</Col>
+                          <Col md={6}>Sex: &nbsp;&nbsp;&nbsp;&nbsp;{character.sex}</Col>
                         </Row>
                         <Row>
-                          <Col>
-                            <Card className="m-3">
-                              <Card.Header className="text-center">
-                                Nation
-                              </Card.Header>
-                              <Card.Body className="text-center">
-                                {character.nation}
-                              </Card.Body>
-                            </Card>
-                          </Col>
-                          <Col>
-                            <Card className="m-3">
-                              <Card.Header className="text-center">
-                                Tier
-                              </Card.Header>
-                              <Card.Body className="text-center">
-                                {character.tier}
-                              </Card.Body>
-                            </Card>
-                          </Col>
+                          <Col md={6}>Nation: &nbsp;&nbsp;&nbsp;&nbsp;{character.nation}</Col>
+                          <Col md={6}>Age: &nbsp;&nbsp;&nbsp;&nbsp;{character.age}</Col>
                         </Row>
                         <Row>
-                          <Col>
-                          <Card className="m-3">
-                            <Card.Header className="text-center">
-                              Element
-                            </Card.Header>
-                            <Card.Body className="text-center">
-                              {character.element}
-                            </Card.Body>
-                          </Card>
-                          </Col>
+                          <Col md={6}>Race: &nbsp;&nbsp;&nbsp;&nbsp;{character.race}</Col>
+                          <Col md={6}>Position: &nbsp;&nbsp;&nbsp;&nbsp;{character.position}</Col>
+                        </Row>
+                        <Row>
+                          <Col md>Type: &nbsp;&nbsp;&nbsp;&nbsp;{character.type}</Col>
+                          <Col md>Title: &nbsp;&nbsp;&nbsp;&nbsp;{character.title}</Col>
                         </Row>
                       </Card.Body>
                     </Accordion.Collapse>
                   </Card>
+                  <Card>
+                    <Card.Header>
+                      <Accordion.Toggle as={Button} variant="link" eventKey="4">
+                        Stats
+                      </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="4">
+                      <Card.Body>
+                        {/* maxStats: [{
+                              level: 100,
+                              power: 282000,
+                              HP: 6816,
+                              attack: 809,
+                              minAttack: 742,
+                              maxAttack: 877,
+                              defense: 1540,
+                              hit: 100,
+                              dodge: 99,
+                              criticalHit: 120,
+                              block: 210,
+                              attackSpeed: 56,
+                              criticalDamage: 150,
+                              blockDefenceRate: 50,
+                              luck: 0
+                            }]*/}
+                            <ListGroup>
+                              {character.maxStats.map((stats, count) => {
+                                return (
+                                <ListGroup.Item>
+                                  <h5>Level: {stats.level}</h5>
+                                  <hr />
+                                  <Row>
+                                    <Col md={6}>Power: &nbsp;&nbsp;&nbsp;&nbsp;{stats.power}</Col>
+                                    <Col md={6}>HP: &nbsp;&nbsp;&nbsp;&nbsp;{stats.HP}</Col>
+                                  </Row>
+                                  <Row>
+                                    <Col md={6}>Attack: &nbsp;&nbsp;&nbsp;&nbsp;{stats.attack}</Col>
+                                    <Col md={6}>Defense: &nbsp;&nbsp;&nbsp;&nbsp;{stats.defense}</Col>
+                                  </Row>
+                                  <Row>
+                                    <Col md={6}>Hit: &nbsp;&nbsp;&nbsp;&nbsp;{stats.hit}</Col>
+                                    <Col md={6}>Dodge: &nbsp;&nbsp;&nbsp;&nbsp;{stats.dodge}</Col>
+                                  </Row>
+                                  <Row>
+                                    <Col md={6}>Critical Hit: &nbsp;&nbsp;&nbsp;&nbsp;{stats.criticalHit}</Col>
+                                    <Col md={6}>Block: &nbsp;&nbsp;&nbsp;&nbsp;{stats.block}</Col>
+                                  </Row>
+                                  <Row>
+                                    <Col md={6}>Attack Speed: &nbsp;&nbsp;&nbsp;&nbsp;{stats.attackSpeed}</Col>
+                                    <Col md={6}>Critical Damage: &nbsp;&nbsp;&nbsp;&nbsp;{stats.criticalDamage}</Col>
+                                  </Row>
+                                  <Row>
+                                    <Col md={6}>Block Defense Rate: &nbsp;&nbsp;&nbsp;&nbsp;{stats.blockDefenceRate}</Col>
+                                    <Col md={6}>Luck: &nbsp;&nbsp;&nbsp;&nbsp;{stats.luck}</Col>
+                                  </Row>
+                                </ListGroup.Item>
+                                )
+                              })}
+                            </ListGroup>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
                 </Accordion>
+                </Row>
                 </Col>
+              </Row>
+              {/* Recommendations maybe here */}
+              <Row>
+                <h3 className="mt-4">Comments: </h3>
+                <div>
+                  
+                </div>
+              </Row>
+              <Row>
+                
               </Row>
             </Container>
         )
