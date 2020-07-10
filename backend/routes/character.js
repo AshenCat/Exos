4a8b70061559702ccbe20fb75fa412f61306000d
Klifford Agujar <forddagujar95@gmail.com>
2020-07-10 00:31:12 -0400

    added put route

--- backend/routes/character.js
+++ backend/routes/character.js
@@ -1,11 +1,11 @@
 let express = require('express');
 
 let server = express.Router();
-let character = require('../models/character')
+let model = require('../models/character')
 
 server.route('/')
     .get((req, res, next)=>{
-        character.find({}).then((doc, err)=>{
+        model.find({}).then((doc, err)=>{
             if(err) {
                 res.status(200)
                 next(err);
@@ -17,13 +17,35 @@ server.route('/')
         })
     })
     .put((req, res, next) => {
-        if(character.name === null || character.role === null || character.nation === null || character.tier === "-----" ||
-         character.element === "-----" || character.race === null || character.position === null || character.type === null ||
-         character.skills.passive.name === null || character.passive.description === [])
-        console.log("err")
-        console.log(req.data)
-        //character.create({})
-        res.json("aw")
+        const character = req.body
+        // console.log(character)
+        if(character === null || character.name === "" || character.role === "" || character.nation === "" || character.tier === "-----" ||
+         character.element === "-----" || character.race === "" || character.position === "" || character.type === ""){
+            // console.log("ewwww")
+            res.json({msg: "Check character info", payload: null})
+         }
+        else if(character.skills.passive.name === "" || character.skills.passive.description === [] || character.skills.active1.name === "" ||
+         character.skills.active1.cost === "" || character.skills.active1.cost > 5 || character.skills.active1.cost < 0 || character.skills.active1.description === [] || character.skills.active2.name === "" ||
+         character.skills.active2.cost === "" || character.skills.active2.cost > 5 || character.skills.active2.cost < 0 || character.skills.active2.description === []){
+            // console.log("arghhh")
+            res.json({msg: "check character skills", payload: null})
+         }
+        else{
+            console.log(req.data);
+            model.create(character, (err, data) =>{
+                if(err) {
+                    res.status(500);
+                    // console.log("----------------------------------------------------")
+                    // console.log(err);
+                    // console.log("----------------------------------------------------")
+                    next(err);
+                }
+                else res.json({
+                    msg: "Successfully added new character",
+                    payload: data.name
+                })
+            })
+        }
     })
 
 
