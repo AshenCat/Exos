30e04a9e5727157561312351e575bffa756b32f8
Klifford Agujar <forddagujar95@gmail.com>
2020-07-15 18:15:08 -0400

    added email, avatar and notifications

--- backend/models/user.js
+++ backend/models/user.js
@@ -12,15 +12,22 @@ let userSchema = new Schema({
         type: String,
         required: true
     },
-    // email: {
-    //     type: String,
-    //     unique: true
-    // },
+    email: {
+        type: String,
+        unique: true
+    },
+    avatar: {
+        type: String,
+    },
     access: {
         type: String,
         enum: ["admin", "user", "tech"],
         required: true,
-    }
+    },
+    notifications: [{
+        type: Schema.Types.ObjectId,
+        ref: "comment"
+    }]
 }, 
 {timestamps: true}
 )
